defcc5797cb691fab07252f5380066cc0fd0865d
Mikkel Folting <folti002@umn.edu>
2023-04-13 14:12:56 -0500

    Home page redirect to todos if signed in

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 import NavBar from '@/features/NavBar';
 import { useRouter } from 'next/router';
-import { useAuth } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useAuth } from "@clerk/nextjs";
 import { useEffect } from 'react';
 import MySignInButton from '@/features/MySignInButton';
 
 export default function Home() {
   const router = useRouter();
-  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  const { getToken } = useAuth();
 
-  useEffect(() => {
-    async function process(){
-      // if(userId){
-      //   const token = await getToken({ template: 'codehooks'});
-        
-      //   const result = await fetch()
-      // }
-    }
-    process();
-  }, [isLoaded]);
+  function TodosRedirect(){
+    router.push('/todos');
+  }
 
   return (
     <>
@@ -28,14 +21,22 @@ export default function Home() {
         <title>GeoDo List</title>
         <link rel="icon" href="/GeoDoListLogo.png" />
       </Head>
-      <NavBar />
-      <div className='container'>
-        <Image src='/GeoDoListLogo.png' width={400} height={400} alt='GeoDo List logo'/>
-        <h1 className='title'> Welcome to your GEOgraphy toDO List! </h1>
-        <h3 className='subtitle'> Sign in or create an account below! </h3>
-        <MySignInButton />
-        {/* <Link href='todos'> View your todos </Link> */}
-      </div>
+
+      {/* If signed in, redirect to todos page */}
+      <SignedIn>
+        <TodosRedirect />
+      </SignedIn>
+
+      {/* If signed out, render home page */}
+      <SignedOut>
+        <NavBar />
+        <div className='container'>
+          <Image src='/GeoDoListLogo.png' width={400} height={400} alt='GeoDo List logo'/>
+          <h1 className='title'> Welcome to your GEOgraphy toDO List! </h1>
+          <h3 className='subtitle'> Sign in or create an account below! </h3>
+          <MySignInButton />
+        </div>
+      </SignedOut>
     </>
   );
 }
