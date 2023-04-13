6a6eaae64b08c49c5faf067d3f86d229ddf39518
Mikkel Folting <folti002@umn.edu>
2023-04-12 22:53:58 -0500

    Some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
       <NavBar></NavBar>
       <div className='container'>
         <Image src='/GeoDoListLogo.png' width={400} height={400} alt='GeoDo List logo'/>
-        <h1> Welcome to your GeoDo List! </h1>
-        <h3> Sign in or create an account below! </h3>
+        <h1 className='title'> Welcome to your GEOgraphy toDO List! </h1>
+        <h3 className='subtitle'> Sign in or create an account below! </h3>
         <button> Sign in/create account </button>
         <Link href='todos'> View your todos </Link>
       </div>
