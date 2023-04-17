06d19c2687d82cd33794dd3473c183a21af74268
Mikkel Folting <folti002@umn.edu>
2023-04-17 10:46:10 -0500

    Fixed NavBar

--- src/features/NavBar.js
+++ src/features/NavBar.js
@@ -18,10 +18,16 @@ export default function NavBar() {
               alt='GeoDo List logo'
             />
           </Link>
+
+          {/* <a role='button' class='navbar-burger' aria-label='menu' aria-expanded='false' data-target='geoDoNavBar'>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+          </a> */}
         </div>
 
         {/* Navigation Buttons */}
-        <div id='geoDoNavBar' className='navbar-menu'>
+        <div id='geoDoNavBar' className='navbar-menu is-active' style={{backgroundColor: '#f48c4c'}}>
           <div className='navbar-item'>
             <Link href='/todos' id='bright-hover' className='navbar-item'>
               Todos
@@ -35,9 +41,6 @@ export default function NavBar() {
           {/* Sign-in button or user button if signed in */}
           <div className='navbar-end'>
             <div className='navbar-item'>
-              {/* <SignedOut>
-                <MySignInButton />
-              </SignedOut> */}
               <SignedIn>
                 <div>
                   <UserButton />
